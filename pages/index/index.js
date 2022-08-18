9bef10b3d6fb7760a284c90a7736a6c8ee88fd8d
TGDXAT <2811579869@qq.com>
2022-08-18 22:34:00 +0800

    更新了动态获取locationID

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,11 +2,7 @@
 
 //依赖导入
 //https://juejin.cn/post/7098210302285578253
-const util = require("../../utils/util");
 const apiKey = require("../../utils/apiKey");
-
-// 获取应用实例
-const app = getApp()
 
 Page({
   //页面的初始数据
@@ -22,32 +18,51 @@ Page({
       windDir:0,//风向
       windSpeed:0,//风速，公里/小时
       windScale:0//风力等级
-    }
+    },
+    locationID:'0'
   },
 
   //页面载入完成后执行一次天气更新
   onLoad:function(options){
-    this.getWeather();
+    this.updateWeather(this.getWeather);
   },
 
   //当地区改变后执行一次天气更新
   bindRegionChange(e){
     this.setData({region:e.detail.value});
-    this.getWeather();
+    this.updateWeather(this.getWeather);
   },
-  
-  //获取天气
-  getWeather(){
+
+  //更新天气
+  updateWeather(getWeather){
+    //获取locationID，通过和风天气的api
     let that = this;
+    wx.request({
+      url: 'https://geoapi.qweather.com/v2/city/lookup',
+      data:{
+        location:that.data.region[2].slice(0,2),
+        key:apiKey.getkey()
+      },
+      success(res){
+        //更新页面数据中的locationID
+        that.setData({locationID:res.data.location[0].id});
+        //根据locationID获取天气信息
+        getWeather(that,that.data.locationID);
+      }
+    })
+  },
+
+  //获取天气
+  getWeather(that,locationID){
     wx.request({
       url: 'https://devapi.qweather.com/v7/weather/now',
       data:{
-        location:util.getLocationID(that.data.region[2].slice(0,2)),//在视图层picker中获取区的信息，再删除“区”字，例如：“崂山”区，处理完后得到“崂山”，再将“崂山”传递给utils的util里的getLocationID函数，得到“崂山”对应的区号101120202，放入请求中
-        key:apiKey.getkey()//获取apiKey
+        location:locationID,
+        key:apiKey.getkey()
       },
-      success:function(res){
-        that.setData({now:res.data.now})
+      success(res){
+        that.setData({now:res.data.now});
       }
-    })
+    });
   }
 })
